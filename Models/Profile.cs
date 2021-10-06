0d55c0116b6ae0e4f6b3d56d7e4888c4a2f1529e
Marshall Scheiderer <ms963717@ohio.edu>
2021-10-06 11:30:29 -0400

    created the first model for profile creation

---
+++ Models/Profile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200Team5.Models
+{
+    public class Profile
+    {
+        public int ProfileID { get; set; }
+        public string ProfileFirst { get; set; }
+        public string ProfileLast { get; set; }
+        public DateTime ProfileBday { get; set; }
+        public string ProfileEmail { get; set; }
+        public string ProfileJobTitle { get; set; }
+        
+    }
+}
